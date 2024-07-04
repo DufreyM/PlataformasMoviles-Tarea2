578fcd5cfcb5100fb15e1e123c6450aa87bee268
GAOV314 <oca231270@uvg.edu.gt>
2024-07-04 11:55:08 -0600

    Metodos calculadora

---
+++ src/Calc.java
@@ -0,0 +1,20 @@
+
+public class Calc{
+
+    public Calc(){}
+
+    public int suma(int n1, int n2){
+        int resultado = n1 + n2;
+        return resultado;
+    }
+
+    public int resta(int n1, int n2){
+        int resultado = n1 - n2;
+        return resultado;
+    }
+
+    public int multiplicacion(int n1, int n2){
+        int resultado = n1*n2;
+        return resultado;
+    }
+}
